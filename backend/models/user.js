ec0934a784c6e56516c821d7aff197c53f05118f
dodamartin <146016722+dodamartin@users.noreply.github.com>
2023-10-23 14:00:54 +0200

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
 
+const UserSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+  photo: String,
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
